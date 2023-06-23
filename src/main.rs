8280b71cc78dcaafa5c810682c5ce4d45b1680e8
Marius Becker <marius.becker.8@gmail.com>
2023-06-23 23:55:16 +0200

    Clean up code a bit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::io;
 use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::path::Path;
 use std::path::PathBuf;
@@ -7,29 +8,29 @@ use std::path::PathBuf;
 const SCRIPT_REGION: &str = "Script";
 
 fn get_scripts_path() -> PathBuf {
-    let appdata = std::env::var("APPDATA").unwrap();
+    let appdata = std::env::var("APPDATA").expect("env var APPDATA should be set on Windows");
     let s = format!("{appdata}\\SpaceEngineers\\IngameScripts\\local");
     PathBuf::from(s)
 }
 
-fn get_project_name() -> String {
-    env::current_dir()
-        .unwrap()
-        .file_name()
-        .unwrap()
-        .to_string_lossy()
-        .to_string()
+fn get_project_name() -> Option<String> {
+    Some(
+        env::current_dir()
+            .ok()?
+            .file_name()?
+            .to_string_lossy()
+            .to_string(),
+    )
 }
 
-fn copy_script<S: AsRef<Path>, T: AsRef<Path>>(source_path: S, target_path: T) {
-    let source_file = BufReader::new(fs::File::open(source_path).unwrap());
+fn copy_script<S: AsRef<Path>, T: AsRef<Path>>(source_path: S, target_path: T) -> io::Result<()> {
+    let source_file = BufReader::new(fs::File::open(source_path)?);
     let mut target_file = BufWriter::new(
         fs::OpenOptions::new()
             .create(true)
             .write(true)
             .truncate(true)
-            .open(target_path)
-            .unwrap(),
+            .open(target_path)?,
     );
 
     let mut regions: Vec<String> = Vec::new();
@@ -60,33 +61,44 @@ fn copy_script<S: AsRef<Path>, T: AsRef<Path>>(source_path: S, target_path: T) {
 
         if in_content {
             if line.len() > content_indent {
-                target_file
-                    .write_all(line[content_indent..].as_bytes())
-                    .unwrap();
-                target_file.write_all(b"\n").unwrap();
+                target_file.write_all(line[content_indent..].as_bytes())?;
+                target_file.write_all(b"\n")?;
             } else {
-                target_file.write_all(b"\n").unwrap();
+                target_file.write_all(b"\n")?;
             }
         }
     }
+
+    Ok(())
 }
 
 fn main() {
-    let project_name = get_project_name();
+    let Some(project_name) = get_project_name() else {
+        println!("Could not determine script name. Is the current directory readable?");
+        return;
+    };
     let mut scripts_path = get_scripts_path();
     scripts_path.push(project_name.as_str());
 
     let mut target_path = scripts_path.clone();
     target_path.push("script.cs");
 
-    let mut source_path = env::current_dir().unwrap();
+    let Ok(mut source_path) = env::current_dir() else {
+        println!("Could not determine working directory");
+        return;
+    };
     source_path.push("Script.cs");
 
-    if !scripts_path.exists() {
-        fs::create_dir_all(&scripts_path).unwrap();
+    if !scripts_path.exists() && fs::create_dir_all(&scripts_path).is_err() {
+        println!(
+            "Could not create target directory {}",
+            scripts_path.to_str().unwrap()
+        );
+        return;
     }
 
-    copy_script(&source_path, &target_path);
-
-    println!("Script written to {}", scripts_path.to_str().unwrap());
+    match copy_script(&source_path, &target_path) {
+        Ok(_) => println!("Script written to {}", scripts_path.to_str().unwrap()),
+        Err(_) => println!("Could not copy script."),
+    }
 }
